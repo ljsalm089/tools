01e73b37e7087556555346b8b518c8bfc719cc67
Json.Lee <ljsalm089@gmail.com>
2018-11-15 15:30:36 +0800

    add method to get extra info in v2 signing block

--- Android/ApkV2ChannelTools/app/src/main/java/cn/ljs/apkv2channeltools/MainActivity.java
+++ Android/ApkV2ChannelTools/app/src/main/java/cn/ljs/apkv2channeltools/MainActivity.java
@@ -8,7 +8,6 @@ import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
-import android.widget.Toast;
 
 import cn.ljs.apkv2channeltools.utils.APKUtils;
 
@@ -18,18 +17,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        FrameLayout frameLayout = new FrameLayout(this);
+        setContentView(frameLayout, new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
+
         AppCompatTextView textView = new AppCompatTextView(this);
         FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT);
         params.gravity = Gravity.CENTER;
-        textView.setTextColor(Color.WHITE);
+        textView.setTextColor(Color.BLACK);
         textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
 
-        setContentView(textView, params);
+        frameLayout.addView(textView, params);
         try {
             textView.setText(String.format("APP Channel : %s", APKUtils.getApkV2ChannelInfo(this)));
         } catch (APKUtils.ChannelNotFoundException e) {
-            Toast.makeText(this, "APP Channel Not Found", Toast.LENGTH_LONG).show();
+            textView.setText("APP Channel not found!");
+        }
+
+        AppCompatTextView extraTv = new AppCompatTextView(this);
+        params = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.gravity = Gravity.BOTTOM | Gravity.CENTER;
+        extraTv.setTextColor(Color.BLACK);
+        extraTv.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
+        frameLayout.addView(extraTv, params);
+
+        try {
+            extraTv.setText(String.format("APP Extra Info : %s", APKUtils.getApkExtraInfo(
+                    this, "channel")));
+        } catch (APKUtils.ApkExtraInfoNotFoundException e) {
+            extraTv.setText("APP Extra Info not found");
         }
     }
+
 }
